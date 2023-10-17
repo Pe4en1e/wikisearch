a5857a1901119276968337acef9ac489feb58184
Roberts Vesīks <alex36254@gmail.com>
2023-10-17 17:37:25 +0300

    final

--- main.py
+++ main.py
@@ -15,27 +15,27 @@ def deafult():
     return "БВТ2304 Дубровин Александр"
 
 
-
+# Path 
 @app.get('/path/{page}', description='Возвращает резюме статьи')
 def path(page: str):
     return wikiworker.getSummary(page, None)
 
-
+# Querry
 @app.get('/query', description='Возвращает резюме статьи, можно указать кол-во предложений ')
 def query(page: str, sent: int):
     return wikiworker.getSummary(page, sent)
 
-
+# Формирование тела ответа
 class WikiAnswer(BaseModel):
     title: str
     content: str
     link: str
 
-
+# Формирование тела запроса
 class WikiInput(BaseModel):
     title: str
 
-
+# Post запрос
 @app.post('/post', response_model=WikiAnswer, description='Возвращает название, полное содержание и ссылку на статью')
 def request(input: WikiInput):
     wikipage = wikiworker.getPage(input.title)
